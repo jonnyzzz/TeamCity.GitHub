9853b23be0587437644852c3a115f944597fa100
Eugene Petrenko <eugene.petrenko@gmail.com>
2014-04-20 18:55:29 +0200

    @NotNull added, #54

--- server/src/jetbrains/teamcilty/github/api/impl/GitHubApiPasswordAuthentication.java
+++ server/src/jetbrains/teamcilty/github/api/impl/GitHubApiPasswordAuthentication.java
@@ -2,16 +2,18 @@ package jetbrains.teamcilty.github.api.impl;
 
 import jetbrains.teamcilty.github.api.GitHubApiAuthentication;
 import org.apache.http.auth.UsernamePasswordCredentials;
+import org.jetbrains.annotations.NotNull;
 
 public class GitHubApiPasswordAuthentication implements GitHubApiAuthentication {
   private final String myUsername;
   private final String myPassword;
 
-  public GitHubApiPasswordAuthentication(String username, String password) {
+  public GitHubApiPasswordAuthentication(@NotNull final String username, @NotNull final String password) {
     myUsername = username;
     myPassword = password;
   }
 
+  @NotNull
   public UsernamePasswordCredentials buildCredentials() {
     return new UsernamePasswordCredentials(myUsername, myPassword);
   }
--- server/src/jetbrains/teamcilty/github/api/impl/GitHubApiTokenAuthentication.java
+++ server/src/jetbrains/teamcilty/github/api/impl/GitHubApiTokenAuthentication.java
@@ -2,14 +2,16 @@ package jetbrains.teamcilty.github.api.impl;
 
 import jetbrains.teamcilty.github.api.GitHubApiAuthentication;
 import org.apache.http.auth.UsernamePasswordCredentials;
+import org.jetbrains.annotations.NotNull;
 
 public class GitHubApiTokenAuthentication implements GitHubApiAuthentication {
   private final String myToken;
 
-  public GitHubApiTokenAuthentication(String token) {
+  public GitHubApiTokenAuthentication(@NotNull final String token) {
     myToken = token;
   }
 
+  @NotNull
   public UsernamePasswordCredentials buildCredentials() {
     return new UsernamePasswordCredentials(myToken, "x-oauth-basic");
   }
